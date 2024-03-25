ce080437adc0ba9c7c3312d1a6fc12be6e55c0cb
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-03-25 08:23:46 +0300

    added files

---
+++ src/Main.kt
@@ -0,0 +1,53 @@
+//Write and invoke a function that given the string “Barnie bakes brown bagels
+//and buns” prints out a list of all the other words and characters making up the
+//string excluding all possible occurrences of the letter b.
+
+fun main(){
+  var sentence: "Barnie bakes brown bagels and buns"
+}
+
+fun printstring(){
+    var sentence: ("Barnie bakes brown bagels and buns")
+    sentence.slice("b")
+    println(sentence)
+
+}
+
+//Write and invoke one function that takes in an array of integers and returns
+//these 3 values: sum, count and average of all the elements.
+fun numbers(){
+    var (num1:Int ,num2:Int):Int{
+        var add
+        var count
+        var average
+
+    }
+
+    }
+
+//  The volume of a sphere is calculated using the formula below
+//
+//V = 4/3 π r
+//3
+//
+//Write and invoke a function that is capable of accurately calculating the
+//volume of any sphere given its radius. Use 3.14159 as π
+fun volume(){
+ var number:3.14159
+ println()
+}
+
+//A palindrome is a string that reads the same forwards and backwards e.g
+//madam, wow, kayak. Write and invoke a function:
+//isPalindrome(word: String): Boolean
+//that takes in a single word and returns true or false depending on whether the
+//word is a palindrome.
+
+fun isPalindrome(word:String): Boolean{
+    var word: madam;
+    if (madam);
+    println(true);
+    else()
+        println(false)
+    println(word)
+}
